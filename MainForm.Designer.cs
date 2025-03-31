83d0016c50b476a06faf12927654680b2ecd13c2
KhanhLe2710 <leducbaokhanh2710@gmail.com>
2025-03-31 22:13:36 +0700

    Khởi tạo dự án Windows Forms với MainForm

---
+++ MainForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ContactList
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            btnAddContact = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(396, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(125, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Danh sách liên hệ";
+            // 
+            // btnAddContact
+            // 
+            btnAddContact.Location = new Point(396, 83);
+            btnAddContact.Name = "btnAddContact";
+            btnAddContact.Size = new Size(125, 29);
+            btnAddContact.TabIndex = 1;
+            btnAddContact.Text = "Thêm liên hệ";
+            btnAddContact.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnAddContact);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnAddContact;
+    }
+}
